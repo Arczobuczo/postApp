8a85ee4db4d28fd66b5b070ff392e6d84c6b31f9
Arczo <arturboratynski@gmail.com>
2020-11-14 12:20:05 +0100

    addslider

--- src/app/component/posts-list/posts-list.component.ts
+++ src/app/component/posts-list/posts-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Post} from '../../model/post.modul';
 
 @Component({
   selector: 'app-posts-list',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-list.component.css']
 })
 export class PostsListComponent implements OnInit {
+
+  public posts: Post[] = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'ipsmum',
+      body: 'lorem'
+    },
+    {
+      userId: 2,
+      id: 2,
+      title: 'ipsmum2',
+      body: 'lorem2'
+    }
+  ];
 
   constructor() { }
 
